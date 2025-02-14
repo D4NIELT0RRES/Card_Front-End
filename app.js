b7761744b9a3f9782ba88b44928e8f931ea41bbc
danieltorres <161612167+D4NIELT0RRES@users.noreply.github.com>
2025-02-13 21:04:16 -0300

    terminando cards

--- app.js
+++ app.js
@@ -29,44 +29,49 @@ function criarMenu(disciplina){
 
 }
 
-const item = [
+const produto = [
 
     {
         nome:'Back-End',
         descricao:'Aprenda a criar APIs, integrar bancos de dados e gerenciar servidores com Node.js, Python ou Java.',
         photo:'backend.png',
-        valor:'R$599,00',
+        valor:'R$179,90',
         duracao:'Duração',
         modulos: ['M1','M2','M3','M4'],
-        cart:'ADD TO CART'
+        cart:'ADD TO CART',
+        link: 'https://www.udemy.com/course/web-completo/?audience=Keyword&campaigntype=Search&funnel=Conversion&gad_source=1&gclid=CjwKCAiAzba9BhBhEiwA7glbas5cUvmSh7NrfPxaqx2z9-UfRWXVr2ejtypCDX_lPiBMg6bDb5mKdxoC1NUQAvD_BwE&language=PT&matchtype=b&portfolio=Brazil&priority=Beta&product=Course&test=&topic=WebDevelopment&utm_campaign=MX_FF-CONV_BR_Search-NB_Keyword_Beta_la.PT_Google&utm_content=&utm_medium=udemyads&utm_source=adwords&utm_term=_._ag_164341648705_._ad_706273691404_._kw_desenvolvimento+web+back+end_._de_c_._dm__._pl__._ti_kwd-940650276447_._li_9196805_._pd__._&couponCode=2021PM25'
 
     },
     {
         nome:'Front-End',
         descricao:'Desenvolva interfaces modernas com HTML, CSS, JavaScript e frameworks como React ou Vue.js.',
-        photo:'Front.png',
-        valor:'R$790,00',
+        photo:'Front-end.png',
+        valor:'R$239,90',
         duracao:'Duração',
         modulos: ['M1','M2','M3','M4'],
-        cart:'ADD TO CART' 
+        cart:'ADD TO CART',
+        link: 'https://www.udemy.com/course/curso-web-design-fundamentos-aprenda-html-css-e-javascript/?couponCode=2021PM25'
     },
     {
-        nome:'Mobile',
-        descricao:'Crie apps para Android e iOS com Flutter, React Native, Swift ou Kotlin.',
+        nome:'Projetos',
+        descricao:'Domine metodologias ágeis (Scrum, Kanban) e ferramentas como Trello e Jira.',
         photo:'kotlin.png',
-        valor:'R$600,00',
+        valor:'R$169,90',
         duracao:'Duração',
         modulos: ['M1','M2','M3','M4'],
-        cart:'ADD TO CART'
+        cart:'ADD TO CART',
+        link: 'https://www.udemy.com/course/planejamento-gestao-de-projetos-tradicional-pmi-completo/?couponCode=2021PM25'
     },
     {
         nome:'Mobile',
         descricao:'Crie apps para Android e iOS com Flutter, React Native, Swift ou Kotlin.',
         photo:'android.png',
-        valor:'R$600,00',
+        valor:'R$219,90',
         duracao:'Duração',
         modulos: ['M1','M2','M3','M4'],
-        cart:'ADD TO CART'
+        cart:'ADD TO CART',
+        link: 'https://www.udemy.com/course/curso-de-desenvolvimento-android-oreo/?couponCode=2021PM25'
+
     }
 ]
 
@@ -83,13 +88,11 @@ function criarCard (produtos){
 
     const texto = document.createElement('div')
     texto.className = 'texto'
-
     const textoH1 = document.createElement('h1')
     
 
     const descricao = document.createElement('div')
     descricao.className = 'descricao'
-
     const descricaoP = document.createElement('p')
 
 
@@ -97,7 +100,6 @@ function criarCard (produtos){
 
     const imagem = document.createElement('div')
     imagem.className = 'imagem'
-
     const img = document.createElement('img')
 
     //Adicionando DIV de modificacao
@@ -107,49 +109,62 @@ function criarCard (produtos){
 
     const valor = document.createElement('div')
     valor.className = 'valor'
-
     const valorH1 = document.createElement('h1')
-
-
-    //Adicionando DIV de subtitulo
 
     const subtitulo = document.createElement('div')
     subtitulo.className = 'subtitulo'
-
     const subtituloH2 = document.createElement('h2')
 
+    const periodo = document.createElement('div')
+    periodo.className = 'periodo'
 
-    //Adicionando DIV carrinho
+    const periodos = document.createElement('div')
+    periodos.className = 'periodos'
+    const periodosP = document.createElement('p')
 
-    const carrinho = document.createElement('div')
-    carrinho.className = 'carrinho'
-
+    const carrinhoLink = document.createElement('a')
+    carrinhoLink.className = 'carrinho'
+    carrinhoLink.href = produtos.link
     const carrinhoH1 = document.createElement('h1')
 
+    
     //Adicionar conteudo na tag
 
     textoH1.textContent = produtos.nome
-    descricao.textContent = produtos.descricao
-    imagem.src=`./img_card/${produtos.imagem}`
+    descricaoP.textContent = produtos.descricao
+    img.src =`./img_card/${produtos.photo}`
+    valorH1.textContent = produtos.valor
+    subtituloH2.textContent = produtos.duracao
+    periodosP.textContent = produtos.modulos.join(',')
+    carrinhoH1.textContent = produtos.cart
+    
+    
+    
 
+    carrinhoH1.textContent = produtos.cart
 
 
     
 
     /***********************************************/
 
-    carrinho.appendChild(carrinhoH1)
-
-    subtitulo.appendChild(subtituloH2)
+    //Adicionando aos pais
+    carrinhoLink.appendChild(carrinhoH1)
+    periodos.appendChild(periodosP)
+    periodo.appendChild(periodos)
+    modificacao.appendChild(periodo)
     valor.appendChild(valorH1)
+    subtitulo.appendChild(subtituloH2)
+    modificacao.appendChild(subtitulo)
     modificacao.appendChild(valor)
+
+
+
+
     imagem.appendChild(img)
-
-
-    descricao.appendChild(descricaoP)
-    texto.appendChild(descricao)
     texto.appendChild(textoH1)
-    textos.appendChild(texto)
+    descricao.appendChild(descricaoP)
+    textos.replaceChildren(texto,descricao)
 
 
 
@@ -157,10 +172,7 @@ function criarCard (produtos){
     card.appendChild(textos)
     card.appendChild(imagem)
     card.appendChild(modificacao)
-    card.appendChild(carrinho)
-    
-
-
+    card.appendChild(carrinhoLink)
     container.appendChild(card)
 }
 
@@ -168,4 +180,4 @@ function criarCard (produtos){
 
 
 disciplinas.forEach(criarMenu)
-item.forEach(criarCard)
+produto.forEach(criarCard)
